fbb45958a5565e539d645194ec525c25593b59ab
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2024-06-01 21:07:26 -0300

    feat: add json-server and submit newsLetter

--- src/app/components/newsletter-form/newsletter-form.component.ts
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 import {
   FormControl,
@@ -6,18 +6,21 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { NewsletterService } from '../../services/newsletter.service';
 
 @Component({
   selector: 'app-newsletter-form',
   standalone: true,
   imports: [BtnPrimaryComponent, ReactiveFormsModule],
+  providers: [NewsletterService],
   templateUrl: './newsletter-form.component.html',
   styleUrl: './newsletter-form.component.scss',
 })
 export class NewsletterFormComponent {
   newsletterForm!: FormGroup;
+  loading = signal(false);
 
-  constructor() {
+  constructor(private service: NewsletterService) {
     this.newsletterForm = new FormGroup({
       name: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -25,6 +28,14 @@ export class NewsletterFormComponent {
   }
 
   onSubmit() {
-    console.log(this.newsletterForm.value);
+    this.loading.set(true);
+    this.service
+      .sendData(this.newsletterForm.value.name, this.newsletterForm.value.email)
+      .subscribe({
+        next: () => {
+          this.newsletterForm.reset();
+          this.loading.set(false);
+        },
+      });
   }
 }
--- src/app/services/newsletter.service.ts
+++ src/app/services/newsletter.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NewsletterService {
+  private endpointUrl = 'http://localhost:3000/newsLetter';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  sendData(
+    name: string,
+    email: string
+  ): Observable<{ name: string; email: string }> {
+    const data = { name, email };
+
+    return this.http.post<{ name: string; email: string }>(
+      this.endpointUrl,
+      data
+    );
+  }
 }
